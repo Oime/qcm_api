a117b683997cebea9e04fed9668d056423bc9f9b
Oime <olivier.gilardi@gmail.com>
2023-11-21 15:38:24 +0100

    init

--- src/start.js
+++ src/start.js
@@ -1,5 +1,13 @@
-const run = async () => {
-  console.log('Vous êtes ici.');
-};
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.checkAll().finally(() => run());
+// Endpoint simple
+app.get('/', (req, res) => {
+    res.send('Bienvenue sur votre API !');
+});
+
+// Écoute du port
+app.listen(port, () => {
+    console.log(`L'API est disponible sur http://localhost:${port}`);
+});
