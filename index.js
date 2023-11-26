21392b70967f720f1e621919512c12848b60f5de
Olivier <olivier.gilardi@gmail.com>
2023-11-27 00:39:43 +0100

    init

--- index.js
+++ index.js
@@ -5,21 +5,40 @@ const app = express();
 app.get('/', (req, res) => res.send('API OK'));
 
 app.get('/getQuestions', (req, res) => {
-    console.log("/getQuestions");
+  console.log("/getQuestions");
+  const sujet = req.query.sujet;
+
+  if (sujet === "ps_nbr") {
     res.send([
-        { question:`Quel est le nombre de côté d'un carré ?`, reponses:[`2`, `3`, `4`, `5`], reponse: `4` },
-        { question:`Quel est la couleur du cheval blanc d'Henry 4 ?`, reponses:[`blanc`, `rouge`], reponse: `blanc` }
+      { question:`Quel est le nombre de côté d'un carré ?`, reponses:[`2`, `3`, `4`, `5`], reponse: `4` },
+      { question:`Quel est la couleur du cheval blanc d'Henry 4 ?`, reponses:[`blanc`, `rouge`], reponse: `blanc` }
     ]);
+  } else if (sujet === "ps_coul") {
+    res.send([
+      { question:`Couleur ?`, reponses:[`2`, `3`, `4`, `5`], reponse: `4` },
+      { question:`Quel est la couleur du cheval blanc d'Henry 4 ?`, reponses:[`blanc`, `rouge`], reponse: `blanc` }
+    ]);
+  } else if (sujet === "cp_math_add") {
+    res.send([
+      { question:`Quel est le nombre de côté d'un carré ?`, reponses:[`2`, `3`, `4`, `5`], reponse: `4` },
+      { question:`Quel est la couleur du cheval blanc d'Henry 4 ?`, reponses:[`blanc`, `rouge`], reponse: `blanc` }
+    ]);
+  } else if (sujet === "CE2_fr_tps") {
+    res.send([
+      { question:`Quel est le nombre de côté d'un carré ?`, reponses:[`2`, `3`, `4`, `5`], reponse: `4` },
+      { question:`Quel est la couleur du cheval blanc d'Henry 4 ?`, reponses:[`blanc`, `rouge`], reponse: `blanc` }
+    ]);
+  }
 });
 
 app.get('/getSujets', (req, res) => {
-    console.log("/getSujets");
-    res.send([
-        { nom:`PS : Je découvre les nombres`, code:`ps_nbr` },
-        { nom:`PS : Je découvre les couleurs`, code:`ps_coul` },
-        { nom:`CP : Math - Les additions`, code:`cp_math_add` },
-        { nom:`CE2 : Français - Les temps`, code:`CE2_fr_tps` },
-    ]);
+  console.log("/getSujets");
+  res.send([
+    { nom:`PS : Je découvre les nombres`, code:`ps_nbr` },
+    { nom:`PS : Je découvre les couleurs`, code:`ps_coul` },
+    { nom:`CP : Math - Les additions`, code:`cp_math_add` },
+    { nom:`CE2 : Français - Les temps`, code:`CE2_fr_tps` },
+  ]);
 });
 
 const port = process.env.PORT || 3000;
